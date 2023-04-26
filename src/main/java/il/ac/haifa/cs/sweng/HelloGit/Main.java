35d17accc2ad947a5b0f12efdc65360e84a7771b
Zoubizaher <zoubizaher@gmail.com>
2023-04-27 02:54:35 +0300

    ADDED NAMES AND LABELS

--- src/main/java/il/ac/haifa/cs/sweng/HelloGit/Main.java
+++ src/main/java/il/ac/haifa/cs/sweng/HelloGit/Main.java
@@ -1,6 +1,6 @@
 package il.ac.haifa.cs.sweng.HelloGit;
 
-public class Main {
+public class Main {// just after this the project worked
     public static void main (String [] args){
         App.main(args);
     }
